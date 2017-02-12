6190779aee0e0799a5ad8fcc2f169574bfc1effe
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2017-02-12 00:19:31 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loader: 'react-hot',
+      exclude: /(node_modules|build)/
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }, {
+      test: /\.js?$/,
+      loader: 'babel',
+      exclude: /(node_modules)/,
+      query: {
+        presets: ['es2015', 'react', 'stage-0'],
+        plugins: ['transform-runtime', 'babel-plugin-transform-flow-strip-types']
+      }
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
+    }]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
